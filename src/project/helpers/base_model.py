f8dec89fe497c192122a31dbf19a0915f5c93d63
JonyR <juan@arzion.com>
2023-03-24 13:59:21 -0300

    Docstrings ok

--- src/project/helpers/base_model.py
+++ src/project/helpers/base_model.py
@@ -6,13 +6,23 @@ from src.project.app import db
 
 
 class BaseModel(object):
+    """
+    Base model for all models to inherit from.
+    """
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     is_deleted = db.Column(db.BOOLEAN, default=False, server_default="0")
 
     def delete(self):
+        """
+        Soft delete the instance.
+        """
         self.is_deleted = True
 
     def undelete(self):
+        """
+        Undelete the instance.
+        """
         self.is_deleted = False
 
     @classmethod
@@ -29,6 +39,10 @@ class BaseModel(object):
 
     @classmethod
     def filters(cls):
+        """
+        Returns a list of tuples with the following format:
+        (column, operator, value, source)
+        """
         return [
             ("is_deleted", "is", False, "default"),
             ("is_active", "is", "is_active", "args"),
@@ -42,60 +56,95 @@ class BaseModel(object):
         return []
 
     def as_dict(self):
-        """Returns instance as a python dictionary."""
+        """
+        Returns a dictionary representation of the model.
+        """
         if hasattr(self, "__table__"):
             return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
 class AuditModel(BaseModel):
+    """
+    Audit model for all models to inherit from.
+    """
+
     created_at = db.Column(
-        db.DateTime(timezone=True), default=datetime.datetime.utcnow, index=True
+        db.DateTime(timezone=True),
+        default=datetime.datetime.utcnow,
+        index=True,
     )
+
     updated_at = db.Column(
-        db.DateTime(timezone=True), default=datetime.datetime.utcnow, index=True
+        db.DateTime(timezone=True),
+        default=datetime.datetime.utcnow,
+        index=True,
     )
-    deleted_at = db.Column(db.DateTime(timezone=True))
+
+    deleted_at = db.Column(
+        db.DateTime(timezone=True),
+    )
 
     def delete(self):
+        """
+        Soft delete the instance.
+        """
         super().delete()
         self.deleted_at = datetime.datetime.utcnow()
 
     def undelete(self):
+        """
+        Undelete the instance.
+        """
         super().undelete()
         self.deleted_at = None
 
     def before_post(self):
+        """
+        Hook to run before a model is created.
+        """
         self.created_at = datetime.datetime.utcnow()
         self.updated_at = datetime.datetime.utcnow()
 
     def before_put(self):
+        """
+        Hook to run before a model is updated.
+        """
         self.updated_at = datetime.datetime.utcnow()
 
     def before_patch(self):
+        """
+        Hook to run before a model is patched.
+        """
         self.updated_at = datetime.datetime.utcnow()
 
     def before_delete(self):
+        """
+        Hook to run before a model is deleted.
+        """
         self.deleted_at = datetime.datetime.utcnow()
-
-    @property
-    def created_at_tz(self):
-        pass
-        # return format_datetime(self.created_at, format="short")
-
-    @property
-    def updated_at_tz(self):
-        pass
-        # return format_datetime(self.updated_at, format="short")
 
 
 class ActiveModel(object):
+    """
+    Active model for all models to inherit from.
+    """
+
     is_active = db.Column(db.BOOLEAN, default=True, server_default="1", index=True)
 
     def toggle(self):
+        """
+        Toggle the active state of the instance.
+        """
         self.is_active = not self.is_active
 
     def enable(self):
+        """
+        Enable the instance.
+        """
         self.is_active = True
 
     def disable(self):
+        """
+        Disable the instance.
+        """
         self.is_active = False
